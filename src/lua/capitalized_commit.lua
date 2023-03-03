407bd7ec2357005373a0102853119e8424b85fb6
siuze <windfall@hnu.edu.cn>
2023-03-03 13:31:19 +0800

    调整lua插件行为
    time_date调整为仅由D+数字触发；
    capitalized_commit在使用Shift+Enter上屏首字母大写的候选项后不切换ascii_mode；
    capitalized_commit仅在选中非首选项时才允许使用Enter上屏候选项

--- src/lua/capitalized_commit.lua
+++ src/lua/capitalized_commit.lua
@@ -5,12 +5,13 @@ local function processor(key_event, env)
             if cand and string.match(cand,'^%a+$') then
                 env.engine:commit_text(cand:gsub("^%l",string.upper))
                 env.engine.context:clear()
+                env.engine.context:set_option('ascii_mode',false)
                 return 1 -- 字符不上屏，结束 processors 流程
             end
         end
     elseif key_event:repr()=="Return" then
         if env.engine.context:has_menu() then
-            if string.match(env.engine.context:get_selected_candidate().text,'^%a+$') then
+            if env.engine.context.composition:back().selected_index > 0 and string.match(env.engine.context:get_selected_candidate().text,'^%a+$') then
                 env.engine.context:commit()
                 env.engine.context:clear()
                 return 1 -- 字符不上屏，结束 processors 流程
--- src/rime.lua
+++
@@ -1,4 +0,0 @@
-capitalized_commit= require("capitalized_commit")
-number_translator= require("number_translator")
-time_date= require("time_date")
-mix_english= require("mix_english")
